55696d39704e0f5aefbc9d11cb5834a84cebba88
oluoko <brianotieno586@gmail.com>
2024-10-16 16:08:18 +0300

    PHP Basics: Classes, Methods & Constructors

---
+++ cryptomasters/converter.php
@@ -0,0 +1,17 @@
+<?php
+class CryptoConverter {
+    // properties
+    // public string $currencyCode;
+
+    // constructor
+     function __construct(public string $currencyCode) {
+        // $this->currencyCode = $currencyCode;
+    }
+
+    //
+
+    // methods
+    public function conver(float $value) {
+
+    }
+}
