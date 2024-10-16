26c76d63f469995b512ced298171c629ccfa1779
oluoko <brianotieno586@gmail.com>
2024-10-16 11:25:03 +0300

    PHP Basics: Creating a Form

---
+++ cryptomasters/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<title>Crypto Masters</title>
+
+<h1>Crypto Masters</h1>
+
+<form >
+    <label for="amount">Amount</label>
+    <input id="amount">
+    <label for="crypto">Crypto Currency</label>
+    <select id="crypto">
+        <option value="BTC">Bitcoin</option>
+        <option value="ETH">Ethereum</option>
+        <option value="LTC">Litecoin</option>
+    </select>
+</form>
+
+
