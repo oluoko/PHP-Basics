bec171cb449cfd7e4f4b1470a00d06892b693a22
oluoko <brianotieno586@gmail.com>
2024-10-22 19:02:19 +0300

    PHP Basics: Handling Null Values & Safe Function Calls

--- cryptomasters/api.php
+++ cryptomasters/api.php
@@ -4,4 +4,4 @@ include("classes.php");
 $code = $_GET['code'] ?? "BTC";
 $converter = new CryptoConverter($code);
 $rateInUSD = $converter->convert();
-echo "{'rate': $rateInUSD}";
+echo "{\"rate\": $rateInUSD}";
