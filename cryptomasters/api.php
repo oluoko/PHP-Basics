11964ae592f90259b9ec5ad97d896ce304b8e743
oluoko <brianotieno586@gmail.com>
2024-10-22 19:28:44 +0300

    PHP Basics: Formatting & Returning JSON Data

--- cryptomasters/api.php
+++ cryptomasters/api.php
@@ -1,6 +1,9 @@
 <?php
 include("classes.php");
 
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+
 $code = $_GET['code'] ?? "BTC";
 $converter = new CryptoConverter($code);
 $rateInUSD = $converter->convert();
